91307d43d9ed890c57f85e9a34f4b046d35e1c55
henry32144 <henry32144@hotmail.com>
2020-02-13 11:25:00 +0800

    Add leaflet map

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import React, { Component, useRef, useState} from 'react';
+import { CSSTransition } from 'react-transition-group';
 import logo from './logo.svg';
 import './App.css';
+import '../node_modules/leaflet/dist/leaflet.css'
+
+// Json datas
+import countyLatLng from '../temp_data/county_latlng.json';
+import geoJsonData from '../temp_data/twCounty2010merge.geo.json';
+
+import countyRemaining from '../temp_data/county_remaining.json';
 
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
@@ -12,14 +20,42 @@ import Popover from 'react-bootstrap/Popover';
 import Container from 'react-bootstrap/Container';
 import ListGroup from 'react-bootstrap/ListGroup';
 import InputGroup from 'react-bootstrap/InputGroup';
+import Modal from 'react-bootstrap/Modal';
 import FormControl from 'react-bootstrap/FormControl';
 import Overlay from 'react-bootstrap/Overlay';
 import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+import Collapse from 'react-bootstrap/Collapse'
 import Accordion from 'react-bootstrap/Accordion';
+import Papa from 'papaparse';
 import { useAccordionToggle } from 'react-bootstrap/AccordionToggle';
-import {FaGithub, FaLinkedin, FaBars, FaInfoCircle} from 'react-icons/fa';
-import { MdRefresh } from 'react-icons/md';
+import {FaGithub, FaLinkedin, FaBars, FaInfoCircle, FaLocationArrow, FaAngleDoubleDown, FaAngleDoubleUp} from 'react-icons/fa';
 import {Typeahead} from 'react-bootstrap-typeahead';
+import L from 'leaflet';
+
+function GeoLocationErrorModal(props) {
+  return (
+    <Modal
+      {...props}
+      size="sm"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          取得座標時發生錯誤
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <p>
+          {props.errorText}
+        </p>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={props.onHide}>Close</Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
 
 class TopNavBar extends Component {
   render() {
@@ -36,43 +72,44 @@ class TopNavBar extends Component {
   }
 }
 
-class RefreshButton extends React.Component {
+class LocateButton extends React.Component {
   render() {
     return (
-      <Button variant="light" className="refresh-button border-left-0" type="button" onClick={this.props.onClick}>
-        <MdRefresh size={this.props.iconSize}/>
+      <Button variant="light" className="side-navbar-locate-button border-left-0" type="button" onClick={this.props.onClick}>
+        <FaLocationArrow size={this.props.iconSize - 2}/>
       </Button>
     )
   }
 }
 
-// class ToggleButton extends React.Component {
-//   render() {
-//     return (
-//       <Button variant="light" className="toogle-button border-0" type="button" onClick={this.props.onClick}>
-//         <FaBars size={this.props.iconSize}/>
-//       </Button>
-//     )
-//   }
-// }
-
-function ToggleButton(props) {
-  const decoratedOnClick = useAccordionToggle(props.eventKey, () =>
-  props.onAccordianToogle()
+function ToogleSideBarButton(props) {
+  const decoratedOnClick = useAccordionToggle(
+    props.eventKey
   );
-
   return (
-    <Button variant="light" className="toogle-button border-0" type="button" onClick={decoratedOnClick}>
+    <Button variant="light" className="side-navbar-expand-button" type="button" onClick={decoratedOnClick}>
       <FaBars size={props.iconSize}/> {props.children}
     </Button>
   );
 }
 
-function CustomTypeahead(props) {
-  const decoratedOnClick = useAccordionToggle(props.eventKey, () =>
-    props.onAccordianToogle()
+function ExpandSideBarButton(props) {
+  const decoratedOnClick = useAccordionToggle(
+    props.eventKey, 
+    () => props.onAccordianToogle()
   );
+  return (
+    <Button variant="light" className="side-navbar-expand-button" type="button" onClick={decoratedOnClick}>
+      {props.isAccordianOpen ? <FaAngleDoubleUp size={props.iconSize}/> : <FaAngleDoubleDown size={props.iconSize}/>} {props.children}
+    </Button>
+  );
+}
 
+function CustomTypeahead(props) {
+  const decoratedOnClick = useAccordionToggle(
+    props.eventKey,
+    () => props.onAccordianToogle()
+  );
   return (
       <Typeahead
         id="id"
@@ -83,7 +120,7 @@ function CustomTypeahead(props) {
         onChange={(selected) => {
           if (selected != null && selected.length > 0) {
             props.onSearchSelected(selected);
-            if (props.isAccordianOpen == false) {
+            if (props.isAccordianOpen === false) {
               decoratedOnClick();
             }
           }
@@ -113,7 +150,7 @@ function SideMenu(props) {
     </ListGroup.Item>
   );
   return (
-    <ListGroup className="side-menu">
+    <ListGroup className="side-navbar-menu-content">
       {listItems}
     </ListGroup>
   )
@@ -122,28 +159,31 @@ function SideMenu(props) {
 class SideNavBar extends Component {
   constructor(props) {
     super(props);
-    this.setBarExpand = this.setBarExpand.bind(this);
+    this.state = {
+      isBarExpand: true,
+      selected: null,
+      isAccordianOpen: false
+    };
+    this.toogleBarExpand = this.toogleBarExpand.bind(this);
     this.onSearchSelected = this.onSearchSelected.bind(this);
-    this.onAccordianToogle = this.onAccordianToogle.bind(this);
-    this.state = {barExpand: true, selected: null, isAccordianOpen: false};
+    this.toogleAccordian = this.toogleAccordian.bind(this);
   }
 
-  setBarExpand(value) {
-    console.log(value.toString());
-    this.setState({
-      barExpand: value
-    });
+  toogleBarExpand() {
+    this.setState(state => ({
+      isBarExpand: !state.isBarExpand
+    }));
   }
 
   onSearchSelected(selected) {
     if (selected != null && selected.length > 0) {
-      this.setState({selected: selected});
-      console.log(selected[0]);
+      this.setState({selected: selected}, () => {
+        console.log(selected[0]);
+      });
     }
   }
 
-  onAccordianToogle() {
-    console.log('onAccordianToogle');
+  toogleAccordian() {
     this.setState(state => ({
       isAccordianOpen: !state.isAccordianOpen
     }));
@@ -159,6 +199,15 @@ class SideNavBar extends Component {
     ];
     return (
       <div className="side-navbar">
+      <CSSTransition
+        in={this.state.isBarExpand}
+        timeout={{
+          enter: 500,
+          exit: 300,
+        }}
+        classNames="side-navbar-transition"
+      >
+      <div className="side-navbar-content">
         <Accordion>
           <Form className="py-3 d-flex justify-content-center">
             <InputGroup>
@@ -168,30 +217,84 @@ class SideNavBar extends Component {
                 selected={this.state.selected}
                 onSearchSelected={this.onSearchSelected}
                 isAccordianOpen={this.state.isAccordianOpen}
-                onAccordianToogle={this.onAccordianToogle} />
+                onAccordianToogle={this.toogleAccordian} />
               <InputGroup.Append>
-                <RefreshButton iconSize={this.props.iconSize}/>
+                <LocateButton iconSize={this.props.iconSize} onClick={this.props.getLocation}/>
               </InputGroup.Append>
             </InputGroup>
-            <ToggleButton eventKey="0" iconSize={this.props.iconSize} onAccordianToogle={this.onAccordianToogle}/>
+            <ExpandSideBarButton 
+              eventKey="0" 
+              iconSize={this.props.iconSize} 
+              onAccordianToogle={this.toogleAccordian}
+              isAccordianOpen={this.state.isAccordianOpen}
+              />
           </Form>
           <Accordion.Collapse eventKey="0">
-            <div className="side-menu-area">
+            <div className="side-navbar-menu">
               <p>衛生局資料最後更新時間: {new Date().toLocaleTimeString()}</p>
               <SideMenu numbers={numbers}/>
             </div>
           </Accordion.Collapse>
         </Accordion>
       </div>
+      </CSSTransition>
+      <Button 
+        className="side-navbar-toogle-button"
+        onClick={this.toogleBarExpand}
+      />
+      </div>
     )
   }
 }
 
-class Map extends Component {
+class Map extends React.Component {
+  constructor(props) {
+    super(props);
+    this.getColor = this.getColor.bind(this);
+    this.drawStyle = this.drawStyle.bind(this);
+  }
+
+  getColor(d) {
+    return d > 20000 ? '#1a9641' :
+           d > 15000  ? '#a6d96a' :
+           d > 10000  ? '#ffffbf' :
+           d > 5000  ? '#fdae61' :
+                      '#d7191c';
+  }
+
+  drawStyle(feature) {
+    var countyName = feature.properties.COUNTYNAME.substring(0, 2);
+    var data = countyRemaining[countyName]["adult_mask"];
+    return {
+        fillColor: this.getColor(data),
+        weight: 2,
+        opacity: 1,
+        color: 'white',
+        dashArray: '3',
+        fillOpacity: 0.7
+    };
+  }
+
+  componentDidMount() {
+    console.log(countyLatLng);
+    // create map
+    this.map = L.map('map', {
+      center: [this.props.userLatitude, this.props.userLongitude],
+      zoom: 8,
+      zoomControl: false,
+      tap: true,
+      dragging: true,
+      layers: [
+        L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+          attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        }),
+      ],
+    });
+    L.geoJSON(geoJsonData, {style: this.drawStyle}).addTo(this.map)
+  }
+
   render() {
-    return (
-      <p>map</p>
-    )
+    return <div id="map"></div>
   }
 }
 
@@ -199,10 +302,14 @@ class MainContents extends Component {
   render() {
     return (
       <div>
-        <SideNavBar iconSize={this.props.iconSize}/>
-        <div className="map">
-          <Map/>
-        </div>
+        <SideNavBar 
+          iconSize={this.props.iconSize}
+          getLocation={this.props.getLocation}
+        />
+        <Map
+          userLatitude={this.props.userLatitude}
+          userLongitude={this.props.userLongitude}
+        />
       </div>
     )
   }
@@ -211,18 +318,82 @@ class MainContents extends Component {
 class App extends Component {
   constructor(props) {
     super(props);
-    this.iconSize = 20;
+    this.state = {
+      iconSize : 20,
+      modalShow : false,
+      userLatitude : 23.583234,
+      userLongitude : 120.5825975,
+      geoLocationErrorText : '您的瀏覽器不支援座標取得功能',
+    };
+    this.handleModalShow = this.handleModalShow.bind(this);
+    this.getLocation = this.getLocation.bind(this);
+    this.handleLocationError = this.handleLocationError.bind(this);
+  }
+
+  handleModalShow(show) {
+    this.setState({modalShow: show});
+  }
+
+  handleLocationError(browserHasGeolocation) {
+    this.setState({
+      geoLocationErrorText: browserHasGeolocation ?
+      '取得座標時發生錯誤. 將顯示預設的地點' :
+      '您的瀏覽器不支援座標取得功能'
+    },
+    () => {
+      this.handleModalShow(true);
+    });
+  }
+
+  getLocation() {
+    Papa.parse("http://127.0.0.1:5000/get-file", {
+      download: true,
+      worker: true,
+      step: function(row) {
+        console.log("Row:", row.data);
+      },
+      complete: function() {
+        console.log("All done!");
+      }
+    });
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        var pos = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        };
+        this.setState({
+          userLatitude : pos.lat,
+          userLongitude : pos.lng
+        });
+      }, () => {
+        this.handleLocationError(true);
+      });
+    } else {
+      // Browser doesn't support Geolocation
+      this.handleLocationError(false);
+    }
   }
 
   render() {
     return (
       <div className="app">
         <div className="top-navbar">
-          <TopNavBar iconSize={this.iconSize}/>
+          <TopNavBar iconSize={this.state.iconSize}/>
         </div>
         <div className="main-contents">
-          <MainContents iconSize={this.iconSize}/>
+          <MainContents 
+            iconSize={this.state.iconSize} 
+            getLocation={this.getLocation}
+            userLatitude={this.state.userLatitude}
+            userLongitude={this.state.userLongitude} 
+          />
         </div>
+        <GeoLocationErrorModal
+          show={this.state.modalShow}
+          errorText={this.state.geoLocationErrorText}
+          onHide={() => {this.handleModalShow(false)}}
+        />
       </div>
     );
   }
